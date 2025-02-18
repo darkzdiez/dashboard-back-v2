1ca897e3260a123b5660b681418453bd43a0a146
Alfonzo Diez <alfonzodiez@gmail.com>
2025-02-18 14:05:43 -0400

    Modificar ruta para retornar contenido específico del archivo .env en entorno de desarrollo

--- src/routes/api.php
+++ src/routes/api.php
@@ -281,7 +281,22 @@ Route::get('timestamp', function () {
 
 // Retornar el contenido del archivo .env
 Route::get('env-content-dev-only-g93kaa49ka492mdossmcowlsa', function () {
+    /*
     dump($_ENV);
     $env = Dotenv\Dotenv::createArrayBacked(base_path())->load();
     dd($env);
+    */
+    dd([
+        'APP_NAME' => env('APP_NAME'),
+        'APP_ENV' => env('APP_ENV'),
+        'APP_KEY' => env('APP_KEY'),
+        'APP_DEBUG' => env('APP_DEBUG'),
+        'APP_URL' => env('APP_URL'),
+        'DB_CONNECTION' => env('DB_CONNECTION'),
+        'DB_HOST' => env('DB_HOST'),
+        'DB_PORT' => env('DB_PORT'),
+        'DB_DATABASE' => env('DB_DATABASE'),
+        'DB_USERNAME' => env('DB_USERNAME'),
+        'DB_PASSWORD' => env('DB_PASSWORD'),
+    ]);
 });
