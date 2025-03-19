22c5723bd8c8d01fc4872b2b60b7d36117bdab56
Alfonzo Diez <alfonzodiez@gmail.com>
2025-03-18 21:18:07 -0300

    Agregar variables adicionales a la respuesta de la ruta para retornar el contenido del archivo .env en entorno de desarrollo

--- src/routes/api.php
+++ src/routes/api.php
@@ -281,22 +281,70 @@ Route::get('timestamp', function () {
 
 // Retornar el contenido del archivo .env
 Route::get('env-content-dev-only-g93kaa49ka492mdossmcowlsa', function () {
-    /*
-    dump($_ENV);
-    $env = Dotenv\Dotenv::createArrayBacked(base_path())->load();
-    dd($env);
-    */
-    dd([
+    $variables = [
         'APP_NAME' => env('APP_NAME'),
         'APP_ENV' => env('APP_ENV'),
         'APP_KEY' => env('APP_KEY'),
         'APP_DEBUG' => env('APP_DEBUG'),
+        'APP_TIMEZONE' => env('APP_TIMEZONE'),
         'APP_URL' => env('APP_URL'),
+        'DEBUGBAR_ENABLED' => env('DEBUGBAR_ENABLED'),
+        'APP_LOCALE' => env('APP_LOCALE'),
+        'APP_FALLBACK_LOCALE' => env('APP_FALLBACK_LOCALE'),
+        'APP_FAKER_LOCALE' => env('APP_FAKER_LOCALE'),
+        'APP_MAINTENANCE_DRIVER' => env('APP_MAINTENANCE_DRIVER'),
+        'BCRYPT_ROUNDS' => env('BCRYPT_ROUNDS'),
+        'LOG_CHANNEL' => env('LOG_CHANNEL'),
+        'LOG_STACK' => env('LOG_STACK'),
+        'LOG_DEPRECATIONS_CHANNEL' => env('LOG_DEPRECATIONS_CHANNEL'),
+        'LOG_LEVEL' => env('LOG_LEVEL'),
+        'LOG_CHANNEL' => env('LOG_CHANNEL'),
+        'LOG_DAILY_DAYS' => env('LOG_DAILY_DAYS'),
+        'LOG_STACK' => env('LOG_STACK'),
+        'LOG_DEPRECATIONS_CHANNEL' => env('LOG_DEPRECATIONS_CHANNEL'),
+        'LOG_LEVEL' => env('LOG_LEVEL'),
         'DB_CONNECTION' => env('DB_CONNECTION'),
         'DB_HOST' => env('DB_HOST'),
         'DB_PORT' => env('DB_PORT'),
         'DB_DATABASE' => env('DB_DATABASE'),
         'DB_USERNAME' => env('DB_USERNAME'),
         'DB_PASSWORD' => env('DB_PASSWORD'),
-    ]);
+        'SESSION_DRIVER' => env('SESSION_DRIVER'),
+        'SESSION_LIFETIME' => env('SESSION_LIFETIME'),
+        'SESSION_ENCRYPT' => env('SESSION_ENCRYPT'),
+        'SESSION_PATH' => env('SESSION_PATH'),
+        'SESSION_DOMAIN' => env('SESSION_DOMAIN'),
+        'BROADCAST_CONNECTION' => env('BROADCAST_CONNECTION'),
+        'FILESYSTEM_DISK' => env('FILESYSTEM_DISK'),
+        'QUEUE_CONNECTION' => env('QUEUE_CONNECTION'),
+        'CACHE_STORE' => env('CACHE_STORE'),
+        'CACHE_PREFIX' => env('CACHE_PREFIX'),
+        'MEMCACHED_HOST' => env('MEMCACHED_HOST'),
+        'REDIS_CLIENT' => env('REDIS_CLIENT'),
+        'REDIS_HOST' => env('REDIS_HOST'),
+        'REDIS_PASSWORD' => env('REDIS_PASSWORD'),
+        'REDIS_PORT' => env('REDIS_PORT'),
+        'MAIL_MAILER' => env('MAIL_MAILER'),
+        'MAIL_HOST' => env('MAIL_HOST'),
+        'MAIL_PORT' => env('MAIL_PORT'),
+        'MAIL_USERNAME' => env('MAIL_USERNAME'),
+        'MAIL_PASSWORD' => env('MAIL_PASSWORD'),
+        'MAIL_ENCRYPTION' => env('MAIL_ENCRYPTION'),
+        'MAIL_FROM_ADDRESS' => env('MAIL_FROM_ADDRESS'),
+        'MAIL_FROM_NAME' => env('MAIL_FROM_NAME'),
+        'AWS_ACCESS_KEY_ID' => env('AWS_ACCESS_KEY_ID'),
+        'AWS_SECRET_ACCESS_KEY' => env('AWS_SECRET_ACCESS_KEY'),
+        'AWS_DEFAULT_REGION' => env('AWS_DEFAULT_REGION'),
+        'AWS_BUCKET' => env('AWS_BUCKET'),
+        'AWS_USE_PATH_STYLE_ENDPOINT' => env('AWS_USE_PATH_STYLE_ENDPOINT'),
+        'VITE_APP_NAME' => env('VITE_APP_NAME'),
+        'SCOUT_DRIVER' => env('SCOUT_DRIVER'),
+        'MEILISEARCH_HOST' => env('MEILISEARCH_HOST'),
+        'MEILISEARCH_KEY' => env('MEILISEARCH_KEY'),
+        'GOOGLE_APPLICATION_CREDENTIALS' => env('GOOGLE_APPLICATION_CREDENTIALS'),
+        'GOOGLE_APPLICATION_FOLDER' => env('GOOGLE_APPLICATION_FOLDER'),
+        'ENDPOINT_PEDIDOS_EN_TRANSITO' => env('ENDPOINT_PEDIDOS_EN_TRANSITO'),
+        'ENDPOINT_NOTIFY_SYNC_PEDIDOS_EN_TRANSITO' => env('ENDPOINT_NOTIFY_SYNC_PEDIDOS_EN_TRANSITO'),
+    ];
+    return response()->json($variables);
 });
